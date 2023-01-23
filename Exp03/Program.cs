6f791352e175e4728d857e48b70be2282bd5584b
Christina <bihtya@mail.ru>
2023-01-23 20:49:51 +0300

    Добавили новые файлы

--- Exp03/Program.cs
+++ Exp03/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = 8;
+if (1 <= number && number <= 7)
+{
+    if (number > 5)
+    {
+        Console.Write("Выходной день");
+    }
+    else
+    {
+        Console.Write("Рабочий день");
+    }
+}
+else
+{
+    Console.Write("К сожалению в календаре нет заданного дня");
+}
